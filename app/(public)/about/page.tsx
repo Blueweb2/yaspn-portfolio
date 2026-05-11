90d8212df3d8265de6c6b78abdfe030d1a4361f9
navaskm <navaskm228@gmail.com>
2026-05-11 16:41:43 +0530

    created introduction section in about page

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,11 +1,13 @@
 import AboutHeroSection from "@/components/about-us/AboutHeroSection"
 import GlobalVision from "@/components/about-us/GlobalVision"
+import Introduction from "@/components/about-us/Introduction"
 
 const page = () => {
   return (
     <>
       <AboutHeroSection />
       <GlobalVision />
+      <Introduction />
     </>
   )
 }
