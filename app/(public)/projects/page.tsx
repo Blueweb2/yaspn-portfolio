1d62fc989555a345fce042ac6c3a84b2d0b86b30
Sabithabasima <sabithabasimavk@gmail.com>
2026-05-12 13:13:05 +0530

    added skyline image in projects

--- app/(public)/projects/page.tsx
+++ app/(public)/projects/page.tsx
@@ -4,12 +4,20 @@ import ProjectsGrid from "@/components/projects/ProjectsGrid";
 
 export default function ProjectsPage() {
   return (
-    <main className="bg-[#162037] text-white">
+    <main className="relative overflow-hidden bg-[#162037] text-white pb-32">
       {/* Hero */}
       <ProjectsHero />
 
       {/* Projects Grid */}
       <ProjectsGrid />
+      {/* Bottom Skyline Cut */}
+      <div className="absolute bottom-0 left-0 w-full">
+        <img
+          src="/skyline-cut-Black.svg"
+          alt="Skyline"
+          className="h-auto w-full object-cover opacity-90"
+        />
+      </div>
     </main>
   );
 }
